0717aed5383f50f7a86bfdfe5eb03c4c653a1b82
BlondeBubblyBryan <bryancwh98@gmail.com>
2020-07-05 20:46:38 +0800

    fix: commented out static storage in settings

--- profiles/migrations/0004_auto_20200614_0837.py
+++ profiles/migrations/0004_auto_20200614_0837.py
@@ -5,7 +5,8 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
+    atomic = False
+    
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('products', '0005_auto_20200613_0718'),

04384ea8c14f7cf8182efdd3ba41d8f9f5ac09e1
BlondeBubblyBryan <bryancwh98@gmail.com>
2020-07-05 21:03:53 +0800

    fix: commented out static storage in settings

--- web/settings.py
+++ web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['tel-me.herokuapp.com/home', '127.0.0.1']
+ALLOWED_HOSTS = ['tel-me.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
